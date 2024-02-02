d7ef8f49ab18cce9c1fec697aa18e5b35b1077d8
Asiaban95 <152020786+Asiaban95@users.noreply.github.com>
2024-02-02 17:16:15 +0330

    Update Human.py

--- Human.py
+++ Human.py
@@ -65,12 +65,12 @@ class Human:
                             
     def incubating(self):
         if (self.E==True):
-            if (random.random()<=.01): # 3 days for incubation periods
+            if (random.random()<=1/3): # 3 days for incubation periods
                 self.E=False
                 self.I=True
     def recovering(self):
         if (self.I==True):
-            if (random.random()<=.01): # 7 days for infectious periods
+            if (random.random()<=1/7): # 7 days for infectious periods
                 self.I=False
                 self.R=True
 
